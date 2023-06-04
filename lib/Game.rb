03b029b9d47d36fae7400a75633c6683501ee2df
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-06-03 18:41:45 -0700

    Complete Game::is_valid_board? method with its tests

---
+++ lib/Game.rb
@@ -0,0 +1,70 @@
+require 'set'
+
+class Game
+  attr_accessor(
+    :rows, :cols, :line_length, :valid_pieces, :current_player_piece,
+    :board, :players, :piece_to_string, :digit_to_string
+  )
+
+  @@valid_pieces = Set.new([:black, :white, :empty])
+
+  @@piece_to_string = {
+    black: "⚫",
+    white: "⚪",
+    empty: "⭕"
+  }
+
+  @@digit_to_string = {
+    1 => "1️⃣", 2 => "2️⃣", 3 => "3️⃣", 4 => "4️⃣",
+    5 => "5️⃣", 6 => "6️⃣", 7 => "7️⃣"
+  }
+
+  def initialize(board = nil, player_1_class = nil, player_2_class = nil)
+    @rows = 6
+    @cols = 7
+    @line_length = 4
+    @current_player_piece = nil
+    @board = board ? self.is_valid_board?(board) : self.get_empty_board
+    @players = []
+
+    @players.push(
+      player_1_class.new(self, "Player #{@players.size + 1}", :black)
+    ) if player_1_class
+
+    @players.push(
+      player_1_class.new(self, "Player #{@players.size + 1}", @players.empty ? :black : :white)
+    ) if player_2_class
+  end
+
+  def add_player(player_class)
+    # TODO
+  end
+
+  private
+
+  def self.is_valid_board?(board, rows = 6, cols = 7)
+    return false if board.class != Array
+    return false if board.size != rows
+    return false if board.any? { |el| el.class != Array }
+    return false if board.any? { |el| el.size != cols }
+
+    board.each do |col|
+      return false if col.any? { |cell| !@@valid_pieces.include?(cell) }
+    end
+
+    last_col = cols - 1
+    last_row = rows - 1
+    second_last_row = rows - 2
+    # returns false if there is a non-empty piece (e.g. black or white)
+    # on "top" of an empty spot in any column because cannot have game
+    # pieces floating on top of air
+    (0..last_col).each do |col|
+      prev = board[last_row][col]
+      (0..second_last_row).reverse_each do |row|
+        return false if prev == :empty && board[row][col] != :empty
+      end
+    end
+
+    true
+  end
+end
---
+++ spec/Game_spec.rb
@@ -0,0 +1,73 @@
+require './lib/Game'
+
+describe Game do
+  describe ".is_valid_board?" do
+    it "returns false if called with a non-array object or value" do
+      expect(Game.is_valid_board?(nil)).to eql(false)
+    end
+
+    it "returns false if called with an array whose size (rows) is not its default value of 6" do
+      expect(Game.is_valid_board?([])).to eql(false)
+    end
+
+    it "returns false if called with an array whose elements are not subarrays" do
+      expect(Game.is_valid_board?(["a","a","a","a","a","a"])).to eql(false)
+    end
+
+    it "returns false if called with an array whose elements are subarrays not of the default column length of 7" do
+      board = [
+        ["a","a","a","a","a","a"],
+        ["a","a","a","a","a","a"],
+        ["a","a","a","a","a","a"],
+        ["a","a","a","a","a","a"],
+        ["a","a","a","a","a","a"],
+        ["a","a","a","a","a","a"],
+      ]
+      expect(Game.is_valid_board?(board)).to eql(false)
+    end
+
+    it "returns false if called with an array whose elements are subarrays of the right size but don't contain valid game pieces" do
+      board = [
+        [nil, nil, nil, nil, nil, nil, :nil],
+        [nil, nil, nil, nil, nil, nil, :nil],
+        [nil, nil, nil, nil, nil, nil, :nil],
+        [nil, nil, nil, nil, nil, nil, :nil],
+        [nil, nil, nil, nil, nil, nil, :nil],
+        [nil, nil, nil, nil, nil, nil, :nil],
+      ]
+      expect(Game.is_valid_board?(board)).to eql(false)
+    end
+
+    it "returns false if called with a 2D array of the right dimensions where there is a non-empty piece symbol not in the bottom-most row of its cell" do
+      board = [
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :white, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :black, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],
+      ]
+      expect(Game.is_valid_board?(board)).to eql(false)
+    end
+
+    it "returns true if called with a 2D array of the right dimensions where every value of its subarray is an :empty piece symbol" do
+      board = [
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],
+      ]
+      expect(Game.is_valid_board?(board)).to eql(true)
+    end
+
+    it "returns true if called with a 2D array of the right dimensions where there are some valid game pieces in the bottom-most row" do
+      board = [
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :black, :white, :white, :empty, :empty, :empty],
+      ]
+      expect(Game.is_valid_board?(board)).to eql(true)
+    end
+
+    it "returns true if called with a 2D array of the right dimensions where there are some valid non-empty game pieces stacked on top of each other" do
+      board = [
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
+        [:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :empty, :empty, :empty, :empty, :empty, :empty],[:empty, :black, :empty, :empty, :empty, :empty, :empty],[:empty, :white, :white, :empty, :empty, :empty, :empty],[:empty, :black, :black, :white, :empty, :empty, :empty],
+      ]
+      expect(Game.is_valid_board?(board)).to eql(true)
+    end
+  end
+end
